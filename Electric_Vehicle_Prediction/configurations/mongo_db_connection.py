0138626fc38b9ddd45367b8763cf340617ca1804
Amir Aziz <Amira@cmrsl.net>
2025-03-26 03:08:20 +0530

    mongodb connection done

--- Electric_Vehicle_Prediction/configurations/mongo_db_connection.py
+++ Electric_Vehicle_Prediction/configurations/mongo_db_connection.py
@@ -4,32 +4,34 @@ import sys
 from pymongo import MongoClient
 from Electric_Vehicle_Prediction.constants import MONGODB_DATABASE_NAME, MONGODB_URI
 from Electric_Vehicle_Prediction.logger import logging
-from Electric_Vehicle_Prediction.exceptions import USvisaException
+from Electric_Vehicle_Prediction.exceptions import EV_Exception
 import certifi
 
 ca = certifi.where()
 
 
-class MongoDBClient:
+class MongoDB_Client:
     client = None
 
     def __init__(self, database_name=MONGODB_DATABASE_NAME) -> None:
         try:
-            if MongoDBClient.client is None:
+            if MongoDB_Client.client is None:
                 mongo_db_url = MONGODB_URI
                 if mongo_db_url is None:
                     raise Exception(f"Environment key: {MONGODB_URI} is not set.")
-                MongoDBClient.client = MongoClient(mongo_db_url, tlsCAFile=ca)
-            self.client = MongoDBClient.client
+                MongoDB_Client.client = MongoClient(mongo_db_url, tlsCAFile=ca)
+            self.client = MongoDB_Client.client
             self.database = self.client[database_name]
             self.database_name = database_name
             logging.info("MongoDB connection succesfull")
         except Exception as e:
-            raise USvisaException(e, sys)
+            raise EV_Exception(e, sys)
+
+    def get_client(self) -> MongoClient:
+        return self.client
 
     def mongodb_collections(self):
         return self.database.list_collection_names()
 
 
-col_name = MongoDBClient().mongodb_collections()
-
+mongodb_client = MongoDB_Client().get_client()
--- Electric_Vehicle_Prediction/data_access/Electric_Vehicle_Prediction_Data_Access.py
+++ Electric_Vehicle_Prediction/data_access/Electric_Vehicle_Prediction_Data_Access.py
@@ -1,1 +1,33 @@
 # Path: Electric_Vehicle_Prediction/data_access/Electric_Vehicle_Prediction_Data_Access.py
+
+from Electric_Vehicle_Prediction.configurations.mongo_db_connection import (
+    mongodb_client,
+)
+from Electric_Vehicle_Prediction.constants import (
+    MONGODB_DATABASE_NAME,
+    MONGODB_COLLECTION_NAME,
+)
+from Electric_Vehicle_Prediction.exceptions import EV_Exception
+import pandas as pd
+import sys
+import numpy as np
+
+
+class EV_Dataframe:
+    def __init__(self):
+        try:
+            self.client = mongodb_client
+            self.database = self.client[MONGODB_DATABASE_NAME]
+            self.collection = self.database[MONGODB_COLLECTION_NAME]
+        except Exception as e:
+            raise EV_Exception(e, sys)
+
+    def export_collection_as_dataframe(self) -> pd.DataFrame:
+        try:
+            data = self.collection.find({}, {"_id": 0, "index": 0})
+            df = pd.DataFrame(list(data))
+            df.drop_duplicates(inplace=True)
+            df.replace({"na": np.nan}, inplace=True)
+            return df
+        except Exception as e:
+            raise EV_Exception(e, sys)
